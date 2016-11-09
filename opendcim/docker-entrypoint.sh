ad268fdf72744e65dd2793ca66fadf6eaa9f5bbe
Karol Kozubal <karol@mevabox.com>
2016-11-09 14:05:43 -0500

    Dockerfile alterations + cleanup

---
+++ opendcim/docker-entrypoint.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+PROCNAME='apache2'
+DAEMON='/usr/sbin/apache2'
+DAEMON_ARGS=( -DFOREGROUND -k start )
+
+if [ -z "$1" ]; then
+  set -- "${DAEMON}" "${DAEMON_ARGS[@]}"
+elif [ "${1:0:1}" = '-' ]; then
+  set -- "${DAEMON}" "$@"
+elif [ "${1}" = "${PROCNAME}" ]; then
+  shift
+  if [ -n "${1}" ]; then
+    set -- "${DAEMON}" "$@"
+  else
+    set -- "${DAEMON}" "${DAEMON_ARGS[@]}"
+  fi
+fi
+
+if [ "$1" = "${DAEMON}" ]; then
+  if [ ! -f /etc/ssl/certs/ssl-cert-snakeoil.pem -o ! -f /etc/ssl/private/ssl-cert-snakeoil.key ]; then
+    dpkg-reconfigure ssl-cert
+  fi
+
+  . /etc/apache2/envvars
+
+  mkdir -p "${APACHE_LOCK_DIR}"
+  mkdir -p "${APACHE_RUN_DIR}"
+
+  rm -f "${APACHE_PID_FILE}"
+fi
+
+exec "$@"
