6ba719f96cc9cede387018da3a14780f7f4eb179
Michael Richard <michael.richard@oriaks.com>
2016-11-12 16:10:52 -0500

    powerdns: fixed api not starting

--- powerdns/docker-entrypoint.sh
+++ powerdns/docker-entrypoint.sh
@@ -4,6 +4,12 @@ PROCNAME='pdns_server'
 DAEMON='/usr/sbin/pdns_server'
 DAEMON_ARGS=( --daemon=no --disable-syslog --guardian=yes --master=yes --setgid=pdns --setuid=pdns --slave=yes )
 
+export PDNS_API_KEY="${PDNS_API_KEY:=}"
+
+if [ -n "${PDNS_API_KEY}" ]; then
+  DAEMON_ARGS+=( --api=yes "--api-key=${PDNS_API_KEY}" --webserver=yes --webserver-address=0.0.0.0 )
+fi
+
 if [ -z "$1" ]; then
   set -- "${DAEMON}" "${DAEMON_ARGS[@]}"
 elif [ "${1:0:1}" = '-' ]; then
@@ -22,11 +28,6 @@ if [ "$1" = "${DAEMON}" ]; then
   export DB_NAME="${DB_NAME:=${MYSQL_ENV_MYSQL_DATABASE}}"
   export DB_PASSWORD="${DB_PASSWORD:=${MYSQL_ENV_MYSQL_PASSWORD}}"
   export DB_USER="${DB_USER:=${MYSQL_ENV_MYSQL_USER}}"
-  export PDNS_API_KEY="${PDNS_API_KEY:=}"
-
-  if [ -n "${PDNS_API_KEY}" ]; then
-    DAEMON_ARGS+=( --api=yes "--api-key=${PDNS_API_KEY}" --webserver=yes --webserver-address=0.0.0.0 )
-  fi
 
   sed -i -f- /etc/powerdns/pdns.d/pdns.gmysql.conf <<- EOF
 	s|^gmysql-dbname=.*|gmysql-dbname=${DB_NAME}|;
