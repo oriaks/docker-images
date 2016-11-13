de504e2f7de4356f8b811e5ac8b791869f9f4928
Michael Richard <michael.richard@oriaks.com>
2016-11-13 09:46:54 -0500

    added ethercalc

---
+++ ethercalc/docker-entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+PROCNAME='ethercalc'
+DAEMON='/usr/bin/ethercalc'
+DAEMON_ARGS=( )
+
+if [ -z "$1" ]; then
+  set -- "${DAEMON}" "${DAEMON_ARGS[@]}"
+elif [ "${1:0:1}" = '-' ]; then
+  set -- "${DAEMON}" "$@"
+elif [ "${1}" = "${PROCNAME}" ]; then
+  shift
+  if [ -n "${1}" ]; then
+    set -- "${DAEMON}" "$@"
+  else
+    set -- "${DAEMON}" "${DAEMON_ARGS[@]}"
+  fi
+fi
+
+if [ "$1" = "${DAEMON}" ]; then
+  export PORT=80
+fi
+
+exec "$@"
