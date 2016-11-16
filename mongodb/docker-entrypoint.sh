65010215df059e391f11e3ae0a75812ac17e569c
Karol Kozubal <karol@mevabox.com>
2016-11-15 22:29:16 -0500

    added mongodb basic standalone image

---
+++ mongodb/docker-entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+PROCNAME='mongod'
+DAEMON='/usr/bin/mongod'
+DAEMON_ARGS=(--config /etc/mongod.conf)
+
+if [ -z "$1" ]; then
+  set -- "${DAEMON}" "${DAEMON_ARGS[@]}"
+elif [ "${1:0:1}" = '-' ]; then
+  set -- "${DAEMON}" "$@"
+elif [ "${1}" = "${PROCNAME}" ]; then
+  shift
+  if [ -n "${1}" ]; then
+    set -- "${DAEMON}" "$@"
+  else
+    set -- "${DAEMON}" "${DAEMON_ARGS[@]}"
+  fi
+fi
+
+if [ "$1" = "${DAEMON}" ]; then
+   # unused for now
+   echo "standalone configuration..."
+fi
+
+exec "$@"
