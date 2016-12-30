2a744c9fadc7dd26efee7c47b570fb0fcf517af0
Michael Richard <michael.richard@oriaks.com>
2016-12-30 17:15:42 -0500

    add cron

---
+++ cron/docker-entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+PROCNAME='cron'
+DAEMON='/usr/sbin/cron'
+DAEMON_ARGS=( -f )
+
+if [ -z "$1" ]; then
+  set -- "${DAEMON}" "${DAEMON_ARGS[@]}"
+elif [ "${1:0:1}" = '-' ]; then
+  set -- "${DAEMON}" "$@"
+elif [ "${1}" = "${PROCNAME}" ]; then
+  shift
+  if [ -n "${1}" ]; then
+    set -- "${DAEMON}" "$@"
+  else
+    set -- "${DAEMON}" "${DAEMON_ARGS[@]}"
+  fi
+fi
+
+exec "$@"
