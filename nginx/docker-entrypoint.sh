920da9f97dec95323d4800f27d19802bc9d341b5
Michael Richard <michael.richard@oriaks.com>
2017-01-03 10:43:33 -0500

    nginx: create symlinks for snakeoil and letsencrypt certificates

--- nginx/docker-entrypoint.sh
+++ nginx/docker-entrypoint.sh
@@ -21,6 +21,18 @@ if [ "$1" = "${DAEMON}" ]; then
   if [ ! -f /etc/ssl/certs/ssl-cert-snakeoil.pem -o ! -f /etc/ssl/private/ssl-cert-snakeoil.key ]; then
     dpkg-reconfigure ssl-cert
   fi
+
+  if [ ! -e /etc/nginx/cert.d/default/cert.pem -o ! -e /etc/nginx/cert.d/default/privkey.pem ]; then
+    mkdir -p /etc/nginx/cert.d/default
+    ln -sf /etc/ssl/certs/ssl-cert-snakeoil.pem /etc/nginx/cert.d/default/cert.pem
+    ln -sf /etc/ssl/private/ssl-cert-snakeoil.key /etc/nginx/cert.d/default/privkey.pem
+  fi
+
+  for CERT in /etc/letsencrypt/live/*; do
+    if [ ! -d "/etc/nginx/cert.d/${CERT##*/}" ]; then
+      ln -s "${CERT}" "/etc/nginx/cert.d/${CERT##*/}"
+    fi
+  done
 fi
 
 exec "$@"
