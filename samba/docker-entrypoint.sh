8055fb76de5db4b687b3cf179326c0288440a331
Michael Richard <michael.richard@oriaks.com>
2017-02-17 15:20:39 +0000

    samba: add support for sasl authentication

--- samba/docker-entrypoint.sh
+++ samba/docker-entrypoint.sh
@@ -19,11 +19,12 @@ fi
 
 if [ "$1" = "${DAEMON}" ]; then
   export SAMBA_DNS_FORWARDER="${SAMBA_DNS_FORWARDER:=8.8.8.8}"
-  export SAMBA_DOMAIN="${SAMBA_DOMAIN:=}"
-  export SAMBA_HOSTNAME="${SAMBA_HOSTNAME:=}"
-  export SAMBA_IP="${SAMBA_IP:=`ip -o addr show dev eth0 scope global | awk -F '[ /]+' '{print $4}'`}"
+  export SAMBA_HOSTNAME="${SAMBA_HOSTNAME:=`hostname -s`}"
+  export SAMBA_IP="${SAMBA_IP:=`hostname -i`}"
   export SAMBA_PASSWORD="${SAMBA_PASSWORD:=}"
-  export SAMBA_REALM="${SAMBA_REALM:=}"
+  export SAMBA_REALM="${SAMBA_REALM:=`hostname -d | tr '[a-z]' '[A-Z]'`}"
+
+  export SAMBA_DOMAIN="${SAMBA_DOMAIN:=`echo "${SAMBA_REALM}" | awk -F '.' '{print $1}'`}"
 
   if [ ! -f /etc/samba/smb.conf ]; then
     samba-tool domain provision \
@@ -32,6 +33,7 @@ if [ "$1" = "${DAEMON}" ]; then
       --host-ip=${SAMBA_IP} \
       --host-name=${SAMBA_HOSTNAME} \
       --option="dns forwarder = ${SAMBA_DNS_FORWARDER}" \
+      --option='ldap server require strong auth = allow_sasl_over_tls' \
       --option='ntlm auth = yes' \
       --realm=${SAMBA_REALM} \
       --use-rfc2307
